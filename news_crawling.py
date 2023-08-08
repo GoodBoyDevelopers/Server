0564c217bae5405041d6c9eca634b6b44dd547b2
yuyi5187 <kimyy200063@gmail.com>
2023-08-08 21:30:46 +0900

    동적 크롤링, todo: 배포시  브라우저 문제 해결

--- news_crawling.py
+++ news_crawling.py
@@ -2,18 +2,23 @@ from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-import time
 import requests
 import os
 import json
 import urllib
+import pyautogui
+import time
 
 
 '''
     keyword 당 관련성 높은 기사 3개의 content, originalLink, summary
-    프론트에서 필요한 것: 이미지, 신문사, 제목, 기사생성일
-    백에서 필요한 것: 기사 원문 텍스트
+    - 프론트에서 필요한 것: 이미지, 신문사, 제목, 기사생성일
+    - 백에서 필요한 것: 기사 원문 텍스트
 '''
 # 
 load_dotenv()
@@ -21,10 +26,48 @@ load_dotenv()
 client_id=os.getenv("X_NAVER_CLIENT_ID")
 client_secret=os.getenv("X_NAVER_CLIENT_SECRET")
 
-def get_summary():
-    pass
+def get_summary(naver_url):
+    summary = {}
+    
+    caps = DesiredCapabilities().CHROME
+    caps['pageLoadStrategy'] = "none"
+    
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    
+    driver = webdriver.Chrome(options= options)
+    driver.get(naver_url)
+    time.sleep(0.5)
 
-def build(soup, original_link, id):
+    sum_button = driver.find_element(By.ID, "_SUMMARY_BUTTON")
+    sum_button.click()
+    #wait(driver, 1).until(EC.find_element(By.ID, "_SUMMARY_BUTTON")).click()
+    time.sleep(0.5)
+    
+    summary_body = driver.find_element(By.CLASS_NAME, '_contents_body._SUMMARY_CONTENT_BODY').text
+    
+    #print(summary_body)
+    title = ""
+    content = ""
+    # 제목, 내용
+    flag = 0 
+    for line in summary_body:
+        if (flag == 0):
+            if (line == '\n'): flag = 1
+            else : title += line
+        else :
+            if (line == '\n') : continue
+            content += line
+    summary['title']= title
+    summary['content'] = content
+    
+    # print (summary )
+    driver.quit()
+    
+    return summary
+
+
+def build(soup, original_link, id, summary):
     '''
     필요한 데이터: 기사 원문, 기사 원본 링크, 네이버 뉴스 요약문
     '''
@@ -65,11 +108,12 @@ def build(soup, original_link, id):
         if line == '\n': continue 
         article += line
     news_info['article']=article
+    news_info['summary']=summary
     
     return news_info
 
 
-def get_originNews(json_data):
+def get_newsinfo(json_data):
     '''
         원본 url -> 원본 기사 추출
     '''
@@ -87,11 +131,10 @@ def get_originNews(json_data):
             if (response.status_code == 200):
                 print("Success")
                 soup = BeautifulSoup(response.text, 'html.parser')
-                news_info = build(soup, original_link, id)
+                summary = get_summary(naver_url)
+                news_info = build(soup, original_link, id, summary)
                 id += 1
                 news.append(news_info)
-                
-                
         except Exception as e :
             print(f"Error Code: {response.status_code}")
             print(e)
@@ -140,14 +183,15 @@ def get_reponseUrl(keyword):
 '''    
 if __name__=='__main__':
     
-    keyword="교육청"
+    keyword="칼"
     json_data = get_reponseUrl(keyword)
     
     if json_data == None:
         print("No Json Data")
     else:
-        res = get_originNews(json_data)
-        print(res)
+        res = get_newsinfo(json_data)
+        for n in res:
+            print(n)
         
 
     
