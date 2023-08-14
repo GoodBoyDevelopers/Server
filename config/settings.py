2a8f28e98f153e1970fb7923295e50d76ae6b2b5
yuyi5187 <kimyy200063@gmail.com>
2023-08-15 00:37:40 +0900

    허어..

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:8000",
     # 배포된 백엔드 주소 추가하기
-    'https://port-0-you-check-ac2nllazbxp3.sel3.cloudtype.app/',
+    'https://port-0-you-check-ac2nllazbxp3.sel3.cloudtype.app',
     'https://you-check:3000',
 ]
 
