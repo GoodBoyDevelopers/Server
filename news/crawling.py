fc4830b515731063c31c59799851ac76d1278638
yuyi5187 <kimyy200063@gmail.com>
2023-08-16 20:51:39 +0900

    build_sports 수정

--- news/crawling.py
+++ news/crawling.py
@@ -83,9 +83,10 @@ def build_sports(soup, origin_link):
     news_info = {}
     # 기사 본문
     try:
-        origin_body = soup.find('div', class_="news_end font1 size3")
+        origin_body = soup.select_one('div #newsEndContents')
     except Exception as e:
         print(e)
+        return False
         
     article = ""
     try:
@@ -96,7 +97,7 @@ def build_sports(soup, origin_link):
     except Exception as e:
         print(e)
         return False
-    
+
     # 제목
     try:
         title = soup.select_one("h4.title").get_text().replace('\n', ' ').replace("\t"," ").replace("\r"," ").replace("\\'", "'").replace('\\"','"').replace("\\", "")
